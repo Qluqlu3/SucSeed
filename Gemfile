6abfca9746c17fb8d4cc4ec96e339f1e3f2d512a
Qluqlu3 <plksmcgof@gmail.com>
2023-12-26 00:41:50 +0900

    fix routes

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.3'
+ruby '2.6.10'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
@@ -38,13 +38,13 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
 end
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'web-console', '>= 3.3.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
@@ -59,23 +59,23 @@ group :test do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
-#bootstrap
+# bootstrap
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails', '~> 4.3.3'
 
-#error
+# error
 gem 'rails-i18n', '~> 5.1'
 
-#暗号化
+# 暗号化
 gem 'bcrypt', '>= 3.1.12'
 
-#画像アップロード
+# 画像アップロード
 gem 'carrierwave', '>= 1.2.3'
 gem 'mini_magick', '>= 4.8.0'
 
-#タグ
+# タグ
 gem 'acts-as-taggable-on', '>= 6.0.0'
 
 gem 'guard-livereload', '>= 2.5.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,175 @@
+# Rails.application.routes.draw do
+#   get '' => 'index#root'
+#   get 'index' => 'index#index'
+#   get 'diary/show'
+#   get 'your_page/your_page'
+
+#   get 'user/password_forgot' => 'user#password_forgot'
+#   get 'user/password_reset' => 'user#password_edit'
+#   get 'user/create' => 'user#regist'
+#   get 'email/certified/:id' => 'user#email_certified_show'
+
+#   get 'my_page/my_page' => 'my_page#my_page'
+#   get 'my_page/update' => 'my_page#show'
+
+#   get 'creator/show' => 'creator#show'
+#   get 'creator/create' => 'creator#create'
+#   get 'creator/edit' => 'creator#edit'
+
+#   #相手ページ
+#   get 'page/creator/:id' => 'your_page#creator_show'
+#   get 'page/heir/:id/' => 'your_page#heir_show'
+
+#   #日記
+#   get 'diary/view' => 'diary#select_diary'
+#   get 'diary/post' => 'diary#regist'
+#   get 'diary/my_diary' => 'diary#my_diary'
+#   get 'diary/show/:id' => 'diary#your_diary'
+#   get 'diary/heir/favorite' => 'diary#heir_favorite_diary'
+
+#   #メッセージ
+#   get 'message/list' => 'message#view'
+#   get 'message/add/:id' => 'message#message_list_add'
+#   get 'message/history/:id' => 'message#get_history'
+
+#   #ギャラリー
+#   get 'gallery/view/:id' => 'gallery#user_view'
+#   get 'gallery/my_gallery' => 'gallery#my_gallery'
+#   get 'gallery/selected/:id' => 'gallery#selected_gallery'
+#   get 'gallery/favorite' => 'gallery#favorite_gallery'
+#   get 'gallery/heir/favorite' => 'gallery#heir_favorite_gallery'
+
+#   #後継者ページ
+#   get 'heir/show' => 'heir#heir_show'
+#   get 'heir/edit' => 'heir#heir_edit'
+
+#   #マッチ
+#   #アピール
+#   get 'match/appealed/list' => 'match#appealed_list_view'
+#   get 'match/appeal/list_check' => 'match#appeal_check'
+#   get 'match/matching/list' => 'match#matching_list_view'
+#   #スカウト
+#   get 'match/scouted/list' => 'match#scouted_show'
+#   #スカウトした一覧
+#   get 'match/scout/list_check' => 'match#scout_check'
+
+#   #問い合わせ
+#   get 'inquiry/input' => 'inquiry#input_page'
+
+#   #管理者
+#   get 'admin/login' => 'admin#login'
+#   get 'admin/create/07392' => 'admin#create'
+#   get 'admin/index' => 'admin_edit#index'
+#   get 'admin/management/user' => 'admin_edit#user'
+#   get 'admin/user/edit/:id' => 'admin_edit#user_edit_show'
+#   get 'admin/management/diary' => 'admin_edit#diary'
+#   get 'admin/management/diary_comment' => 'admin_edit#diary_comment'
+#   get 'admin/management/gallery' => 'admin_edit#gallery'
+#   get 'admin/management/inquiry' => 'admin_edit#inquiry'
+#   get 'admin/inquiry/detail/:id' => 'admin_edit#inquiry_detail_show'
+
+#   post 'index' => 'user#logout'
+#   post 'search/user' => 'index#search_user'
+
+#   post 'user/login' => 'user#login'
+#   post 'user/create' => 'user#create'
+#   post 'user/password_forgot' => 'user#email_exist'
+#   patch 'user/password_reset' => 'user#password_reset'
+#   post 'user/password_reset' => 'user#password_reset'
+
+#   #メールアドレス認証
+#   post 'email/certified/:id' => 'user#email_certified'
+
+#   patch 'my_page/update' => 'my_page#update'
+
+#   post 'creator/create' => 'creator#create'
+#   patch 'creator/edit' => 'creator#update'
+#   post 'creator/upload' => 'creator#upload'
+#   post 'creator/:id/delete' => 'creator#delete'
+
+#   post 'diary/post' => 'diary#post'
+#   post 'diary/show/:id/good' => 'diary#good'
+#   post 'diary/show/:id/comment' => 'diary#comment'
+#   post 'diary/post/:id/delete' => 'diary#post_delete'
+#   post 'diary/good-js' => 'diary#good'
+
+#   #お気に入り
+#   post 'favorite/:id/add' => 'favorite#add'
+#   post 'favorite/:id/delete' => 'favorite#delete'
+
+#   #ギャラリー
+#   post 'gallery/view' => 'gallery#upload'
+#   post 'gallery/my_gallery' => 'gallery#my_gallery'
+#   post 'gallery/user/search/tag/:id' => 'gallery#search_user_tag'
+#   post 'gallery/selected/comment/:id' => 'gallery#gallery_comment'
+#   post 'gallery/selected/good/:id' => 'gallery#gallery_good'
+
+#   #メッセージ
+#   post 'message/send/:id' => 'message#send_message'
+#   post 'message/add/:id' => 'message#message_list_add'
+#   post 'message/history/:id' => 'message#get_history'
+
+#   #後継者情報登録
+#   post 'heir/create' => 'heir#heir_create'
+#   patch 'heir/update' => 'heir#heir_update'
+
+#   #マッチ
+#   post 'match/send/:id' => 'match#appeal_send'
+#   post 'match/ok/:id' => 'match#appeal_answer_ok'
+#   post 'match/sorry/:id' => 'match#appeal_answer_sorry'
+#   #スカウト
+#   post 'scout/send/:id' => 'match#scout_send'
+#   post 'scout/ok/:id' => 'match#scout_answer_ok'
+#   post 'scout/sorry/:id' => 'match#scout_answer_sorry'
+
+#   post 'inquiry/input' => 'inquiry#send_inquiry'
+
+#   post 'admin/login' => 'admin#login_challenge'
+#   post 'admin/create/user' => 'admin#create_user'
+
+#   post 'admin/user/delete/:id' => 'admin_edit#user_delete'
+#   patch 'admin/user/edit/:id' => 'admin_edit#user_edit'
+#   post 'admin/diary/delete/:id' => 'admin_edit#diary_delete'
+#   post 'admin/diary_comment/delete/:id' => 'admin_edit#diary_comment_delete'
+#   post 'admin/gallery/delete/:id' => 'admin_edit#gallery_delete'
+#   post 'admin/inquiry/detail/:id' => 'admin_edit#inquiry_detail'
+#   post 'admin/inquiry/detail/check/:id' => 'admin_edit#inquiry_detail_check'
+
+#   get '*path', controller: 'application', action: 'render_404'
+# end
 Rails.application.routes.draw do
   get '' => 'index#root'
   get 'index' => 'index#index'
   get 'diary/show'
   get 'your_page/your_page'
 
-  get 'user/password_forgot' => 'user#password_forgot'
-  get 'user/password_reset' => 'user#password_edit'
-  get 'user/create' => 'user#regist'
+  resources :user do
+    # member do
+    #   get 'password_forgot' => 'user#password_forgot'
+    #   get 'password_reset' => 'user#password_edit'
+    #   get 'create' => 'user#regist'
+
+    #   post 'login' => 'user#login'
+    #   post 'create' => 'user#create'
+    #   post 'password_forgot' => 'user#email_exist'
+    #   patch 'password_reset' => 'user#password_reset'
+    #   post 'password_reset' => 'user#password_reset'
+    # end
+    collection do
+      get 'password_forgot' => 'user#password_forgot'
+      get 'password_reset' => 'user#password_edit'
+      get 'create' => 'user#regist'
+
+      post 'login' => 'user#login'
+      post 'create' => 'user#create'
+      post 'password_forgot' => 'user#email_exist'
+      patch 'password_reset' => 'user#password_reset'
+      post 'password_reset' => 'user#password_reset'
+    end
+  end
+
+  # resources :users do
+  # end
   get 'email/certified/:id' => 'user#email_certified_show'
 
   get 'my_page/my_page' => 'my_page#my_page'
@@ -16,47 +179,64 @@ Rails.application.routes.draw do
   get 'creator/create' => 'creator#create'
   get 'creator/edit' => 'creator#edit'
 
-  #相手ページ
+  # 相手ページ
   get 'page/creator/:id' => 'your_page#creator_show'
   get 'page/heir/:id/' => 'your_page#heir_show'
 
-  #日記
+  # 日記
   get 'diary/view' => 'diary#select_diary'
   get 'diary/post' => 'diary#regist'
   get 'diary/my_diary' => 'diary#my_diary'
   get 'diary/show/:id' => 'diary#your_diary'
   get 'diary/heir/favorite' => 'diary#heir_favorite_diary'
 
-  #メッセージ
-  get 'message/list' => 'message#view'
-  get 'message/add/:id' => 'message#message_list_add'
-  get 'message/history/:id' => 'message#get_history'
+  # メッセージ
+  # get 'message/list' => 'message#view'
+  # get 'message/add/:id' => 'message#message_list_add'
+  # get 'message/history/:id' => 'message#get_history'
+  # scope '/messages', controller: 'message' do
+  #   get 'list', to: 'view'
+  #   get 'add/:id', to: 'message_list_add',
+  #   get 'history/:id', to: 'get_history',
+  # end
+  resources :messages, only: [] do
+    collection do
+      get 'list', to: 'message#view'
+    end
 
-  #ギャラリー
+    member do
+      get 'history/:id', to: 'message#get_history', as: :history
+      get 'add/:id', to: 'message#message_list_add', as: :add
+    end
+  end
+
+  # ギャラリー
   get 'gallery/view/:id' => 'gallery#user_view'
   get 'gallery/my_gallery' => 'gallery#my_gallery'
   get 'gallery/selected/:id' => 'gallery#selected_gallery'
   get 'gallery/favorite' => 'gallery#favorite_gallery'
   get 'gallery/heir/favorite' => 'gallery#heir_favorite_gallery'
 
-  #後継者ページ
-  get 'heir/show' => 'heir#heir_show'
-  get 'heir/edit' => 'heir#heir_edit'
+  # 後継者ページ
+  resources :heirs, only: %i[show edit] do
+    get 'heir/show' => 'heir#heir_show'
+    get 'heir/edit' => 'heir#heir_edit'
+  end
 
-  #マッチ
-  #アピール
+  # マッチ
+  # アピール
   get 'match/appealed/list' => 'match#appealed_list_view'
   get 'match/appeal/list_check' => 'match#appeal_check'
   get 'match/matching/list' => 'match#matching_list_view'
-  #スカウト
+  # スカウト
   get 'match/scouted/list' => 'match#scouted_show'
-  #スカウトした一覧
+  # スカウトした一覧
   get 'match/scout/list_check' => 'match#scout_check'
 
-  #問い合わせ
+  # 問い合わせ
   get 'inquiry/input' => 'inquiry#input_page'
 
-  #管理者
+  # 管理者
   get 'admin/login' => 'admin#login'
   get 'admin/create/07392' => 'admin#create'
   get 'admin/index' => 'admin_edit#index'
@@ -68,17 +248,16 @@ Rails.application.routes.draw do
   get 'admin/management/inquiry' => 'admin_edit#inquiry'
   get 'admin/inquiry/detail/:id' => 'admin_edit#inquiry_detail_show'
 
-
   post 'index' => 'user#logout'
   post 'search/user' => 'index#search_user'
 
-  post 'user/login' => 'user#login'
-  post 'user/create' => 'user#create'
-  post 'user/password_forgot' => 'user#email_exist'
-  patch 'user/password_reset' => 'user#password_reset'
-  post 'user/password_reset' => 'user#password_reset'
+  # post 'user/login' => 'user#login'
+  # post 'user/create' => 'user#create'
+  # post 'user/password_forgot' => 'user#email_exist'
+  # patch 'user/password_reset' => 'user#password_reset'
+  # post 'user/password_reset' => 'user#password_reset'
 
-  #メールアドレス認証
+  # メールアドレス認証
   post 'email/certified/:id' => 'user#email_certified'
 
   patch 'my_page/update' => 'my_page#update'
@@ -94,31 +273,31 @@ Rails.application.routes.draw do
   post 'diary/post/:id/delete' => 'diary#post_delete'
   post 'diary/good-js' => 'diary#good'
 
-  #お気に入り
+  # お気に入り
   post 'favorite/:id/add' => 'favorite#add'
   post 'favorite/:id/delete' => 'favorite#delete'
 
-  #ギャラリー
+  # ギャラリー
   post 'gallery/view' => 'gallery#upload'
   post 'gallery/my_gallery' => 'gallery#my_gallery'
   post 'gallery/user/search/tag/:id' => 'gallery#search_user_tag'
   post 'gallery/selected/comment/:id' => 'gallery#gallery_comment'
   post 'gallery/selected/good/:id' => 'gallery#gallery_good'
 
-  #メッセージ
+  # メッセージ
   post 'message/send/:id' => 'message#send_message'
   post 'message/add/:id' => 'message#message_list_add'
   post 'message/history/:id' => 'message#get_history'
 
-  #後継者情報登録
+  # 後継者情報登録
   post 'heir/create' => 'heir#heir_create'
   patch 'heir/update' => 'heir#heir_update'
 
-  #マッチ
+  # マッチ
   post 'match/send/:id' => 'match#appeal_send'
   post 'match/ok/:id' => 'match#appeal_answer_ok'
   post 'match/sorry/:id' => 'match#appeal_answer_sorry'
-  #スカウト
+  # スカウト
   post 'scout/send/:id' => 'match#scout_send'
   post 'scout/ok/:id' => 'match#scout_answer_ok'
   post 'scout/sorry/:id' => 'match#scout_answer_sorry'
