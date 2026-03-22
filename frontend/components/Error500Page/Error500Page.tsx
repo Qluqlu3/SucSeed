b02a8e1ff2dfb2423fcf83b3470edf73f8bd389b
Qluqlu3 <plksmcgof@gmail.com>
2026-03-22 15:53:43 +0900

    refactor: エラーページのクラス名属性をダブルクォートからシングルクォートに統一

--- frontend/components/Error500Page/Error500Page.tsx
+++ frontend/components/Error500Page/Error500Page.tsx
@@ -4,10 +4,10 @@
 
 export const Error500Page = () => (
   <div>
-    <h1 className="error-title error-page-title">エラーが発生しました</h1>
-    <div className="error-page-box">
-      <div className="error-btn-box">
-        <a href="/index" className="btn btn-lg btn-default">
+    <h1 className='error-title error-page-title'>エラーが発生しました</h1>
+    <div className='error-page-box'>
+      <div className='error-btn-box'>
+        <a href='/index' className='btn btn-lg btn-default'>
           トップページ
         </a>
       </div>
