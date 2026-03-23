f81daf8aafc0b6a6da2c6ea971d77131113fb4aa
Qluqlu3 <plksmcgof@gmail.com>
2026-03-24 00:41:41 +0900

    refactor: JSX内のクラス名属性をダブルクォートからシングルクォートに統一

--- frontend/components/MessagePage/MessagePage.tsx
+++ frontend/components/MessagePage/MessagePage.tsx
@@ -40,28 +40,28 @@ function getCsrfToken(): string {
 
 export const MessagePage = ({ messageLists, messageHistory, fromUser, toUser }: Props) => (
   <div>
-    <h1 className="main-title">メッセージ</h1>
-    <div className="all-cover-box">
-      <div className="wrapper">
-        <div className="row my-row justify-content-center">
+    <h1 className='main-title'>メッセージ</h1>
+    <div className='all-cover-box'>
+      <div className='wrapper'>
+        <div className='row my-row justify-content-center'>
           {/* 左パネル: メッセージリスト */}
-          <div className="col-md-5 my-col-left">
-            <div className="list-box">
+          <div className='col-md-5 my-col-left'>
+            <div className='list-box'>
               {messageLists.map((item) => (
-                <form key={item.id} action={`/message/history/${item.id}`} method="post">
-                  <input type="hidden" name="authenticity_token" value={getCsrfToken()} />
-                  <button type="submit" className="btn message-list-btn">
-                    <div className="message-list-box row my-list-row">
-                      <div className="col-md-3 col-avatar-box">
+                <form key={item.id} action={`/message/history/${item.id}`} method='post'>
+                  <input type='hidden' name='authenticity_token' value={getCsrfToken()} />
+                  <button type='submit' className='btn message-list-btn'>
+                    <div className='message-list-box row my-list-row'>
+                      <div className='col-md-3 col-avatar-box'>
                         <img
                           src={item.avatarPath}
                           width={80}
                           height={80}
-                          className="rounded-circle"
+                          className='rounded-circle'
                           alt={item.name}
                         />
                       </div>
-                      <div className="col-md-9 col-name-box">{item.name}</div>
+                      <div className='col-md-9 col-name-box'>{item.name}</div>
                     </div>
                   </button>
                 </form>
@@ -70,62 +70,62 @@ export const MessagePage = ({ messageLists, messageHistory, fromUser, toUser }: 
           </div>
 
           {/* 右パネル: 履歴 + 送信フォーム */}
-          <div className="col-md-7 my-col-right">
-            <div className="message-box">
-              <h3 className="message-title">{toUser.name}さん</h3>
+          <div className='col-md-7 my-col-right'>
+            <div className='message-box'>
+              <h3 className='message-title'>{toUser.name}さん</h3>
               {messageHistory.map((msg, idx) =>
                 msg.sendUserId === fromUser.id ? (
                   // 自分の発言
-                  <div key={`${msg.createdAt}-${idx}`} className="from-message-box text-right">
-                    <div className="media text-right">
-                      <div className="media-body from-message-content">
+                  <div key={`${msg.createdAt}-${idx}`} className='from-message-box text-right'>
+                    <div className='media text-right'>
+                      <div className='media-body from-message-content'>
                         <p>{msg.content}</p>
                       </div>
                       <img
                         src={fromUser.avatarPath}
                         width={60}
                         height={60}
-                        className="rounded-circle"
-                        alt=""
+                        className='rounded-circle'
+                        alt=''
                       />
                     </div>
-                    <p className="message-time-text">{msg.createdAt}</p>
+                    <p className='message-time-text'>{msg.createdAt}</p>
                   </div>
                 ) : (
                   // 相手の発言
-                  <div key={`${msg.createdAt}-${idx}`} className="to-message-box text-left">
-                    <div className="media text-left">
+                  <div key={`${msg.createdAt}-${idx}`} className='to-message-box text-left'>
+                    <div className='media text-left'>
                       <img
                         src={toUser.avatarPath}
                         width={60}
                         height={60}
-                        className="rounded-circle"
+                        className='rounded-circle'
                         alt={toUser.name}
                       />
-                      <div className="media-body to-message-content">
+                      <div className='media-body to-message-content'>
                         <p>{msg.content}</p>
                       </div>
                     </div>
-                    <p className="message-time-text">{msg.createdAt}</p>
+                    <p className='message-time-text'>{msg.createdAt}</p>
                   </div>
                 ),
               )}
             </div>
 
             {/* 送信フォーム */}
-            <div className="send-message-box">
-              <form action={`/message/send/${toUser.id}`} method="post" className="form-control">
-                <input type="hidden" name="authenticity_token" value={getCsrfToken()} />
-                <div className="input-group">
+            <div className='send-message-box'>
+              <form action={`/message/send/${toUser.id}`} method='post' className='form-control'>
+                <input type='hidden' name='authenticity_token' value={getCsrfToken()} />
+                <div className='input-group'>
                   <input
-                    type="text"
-                    name="message[content]"
-                    className="form-control"
-                    placeholder="メッセージ"
+                    type='text'
+                    name='message[content]'
+                    className='form-control'
+                    placeholder='メッセージ'
                   />
-                  <span className="input-group-btn">
-                    <button type="submit" className="btn send-btn">
-                      <i className="far fa-paper-plane message-icon" />
+                  <span className='input-group-btn'>
+                    <button type='submit' className='btn send-btn'>
+                      <i className='far fa-paper-plane message-icon' />
                     </button>
                   </span>
                 </div>
