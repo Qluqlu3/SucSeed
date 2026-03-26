7a4368dd5eaf53fb66ad1678f4995602f931ba84
Qluqlu3 <plksmcgof@gmail.com>
2026-03-27 02:04:05 +0900

    refactor: JSX内のクラス名属性をダブルクォートからシングルクォートに統一

--- frontend/components/AdminGalleryEditPage/AdminGalleryEditPage.tsx
+++ frontend/components/AdminGalleryEditPage/AdminGalleryEditPage.tsx
@@ -26,39 +26,39 @@ function getCsrfToken(): string {
 }
 
 export const AdminGalleryEditPage = ({ galleries }: Props) => (
-  <div className="row admin-row">
-    <AdminSideMenu activeKey="gallery" />
-    <div className="col-10 admin-col-9">
-      <h1 className="admin-main-title">ギャラリー管理</h1>
-      <table className="table table-striped">
+  <div className='row admin-row'>
+    <AdminSideMenu activeKey='gallery' />
+    <div className='col-10 admin-col-9'>
+      <h1 className='admin-main-title'>ギャラリー管理</h1>
+      <table className='table table-striped'>
         <thead>
           <tr>
-            <th scope="col">＃</th>
-            <th scope="col">ID</th>
-            <th scope="col">ユーザーID</th>
-            <th scope="col">画像</th>
-            <th scope="col">コメント</th>
-            <th scope="col">登録日時</th>
-            <th scope="col">削除日時</th>
+            <th scope='col'>＃</th>
+            <th scope='col'>ID</th>
+            <th scope='col'>ユーザーID</th>
+            <th scope='col'>画像</th>
+            <th scope='col'>コメント</th>
+            <th scope='col'>登録日時</th>
+            <th scope='col'>削除日時</th>
             <th />
           </tr>
         </thead>
         <tbody>
           {galleries.map((gallery, i) => (
             <tr key={gallery.id}>
-              <th scope="row">{i + 1}</th>
+              <th scope='row'>{i + 1}</th>
               <td>{gallery.id}</td>
               <td>{gallery.userId}</td>
               <td>
-                <img src={gallery.data} width={160} height={100} alt="" />
+                <img src={gallery.data} width={160} height={100} alt='' />
               </td>
               <td>{gallery.comment}</td>
               <td>{gallery.createdAt}</td>
               <td>{gallery.deletedAt ?? ''}</td>
               <td>
-                <form action={`/admin/gallery/delete/${gallery.id}`} method="post">
-                  <input type="hidden" name="authenticity_token" value={getCsrfToken()} />
-                  <button type="submit" className="btn btn-danger">
+                <form action={`/admin/gallery/delete/${gallery.id}`} method='post'>
+                  <input type='hidden' name='authenticity_token' value={getCsrfToken()} />
+                  <button type='submit' className='btn btn-danger'>
                     削除
                   </button>
                 </form>
--- frontend/components/AdminInquiryEditPage/AdminInquiryEditPage.tsx
+++ frontend/components/AdminInquiryEditPage/AdminInquiryEditPage.tsx
@@ -25,39 +25,39 @@ interface Props {
 }
 
 export const AdminInquiryEditPage = ({ inquiries }: Props) => (
-  <div className="row admin-row">
-    <AdminSideMenu activeKey="inquiry" />
-    <div className="col-10 admin-col-9">
-      <h1 className="admin-main-title">問い合わせ管理</h1>
-      <table className="table table-striped">
+  <div className='row admin-row'>
+    <AdminSideMenu activeKey='inquiry' />
+    <div className='col-10 admin-col-9'>
+      <h1 className='admin-main-title'>問い合わせ管理</h1>
+      <table className='table table-striped'>
         <thead>
           <tr>
-            <th scope="col">＃</th>
-            <th scope="col">ID</th>
-            <th scope="col">ユーザーID</th>
-            <th scope="col">カテゴリー</th>
-            <th scope="col">内容</th>
-            <th scope="col">対応チェック</th>
-            <th scope="col">登録日時</th>
-            <th scope="col">経過日数</th>
-            <th scope="col">更新日時</th>
-            <th scope="col">削除日時</th>
+            <th scope='col'>＃</th>
+            <th scope='col'>ID</th>
+            <th scope='col'>ユーザーID</th>
+            <th scope='col'>カテゴリー</th>
+            <th scope='col'>内容</th>
+            <th scope='col'>対応チェック</th>
+            <th scope='col'>登録日時</th>
+            <th scope='col'>経過日数</th>
+            <th scope='col'>更新日時</th>
+            <th scope='col'>削除日時</th>
             <th />
           </tr>
         </thead>
         <tbody>
           {inquiries.map((inquiry, i) => (
             <tr key={inquiry.id}>
-              <th scope="row">{i + 1}</th>
+              <th scope='row'>{i + 1}</th>
               <td>{inquiry.id}</td>
               <td>{inquiry.userId}</td>
               <td>{inquiry.name}</td>
               <td>{inquiry.content}</td>
               <td>
                 {inquiry.isCheck ? (
-                  <span className="badge badge-success">OK</span>
+                  <span className='badge badge-success'>OK</span>
                 ) : (
-                  <span className="badge badge-danger">No</span>
+                  <span className='badge badge-danger'>No</span>
                 )}
               </td>
               <td>{inquiry.createdAt}</td>
@@ -65,7 +65,7 @@ export const AdminInquiryEditPage = ({ inquiries }: Props) => (
               <td>{inquiry.updatedAt}</td>
               <td>{inquiry.deletedAt ?? ''}</td>
               <td>
-                <a href={`/admin/inquiry/detail/${inquiry.id}`} className="btn btn-warning">
+                <a href={`/admin/inquiry/detail/${inquiry.id}`} className='btn btn-warning'>
                   チェック
                 </a>
               </td>
