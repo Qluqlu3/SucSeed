a8c10933a3f0dda7877f6fe55a97ec08d58991d5
Qluqlu3 <plksmcgof@gmail.com>
2026-03-28 03:42:24 +0900

    refactor: AdminSelectedUserEditPageのクラス名属性をダブルクォートからシングルクォートに統一

--- frontend/components/AdminSelectedUserEditPage/AdminSelectedUserEditPage.tsx
+++ frontend/components/AdminSelectedUserEditPage/AdminSelectedUserEditPage.tsx
@@ -21,47 +21,47 @@ function getCsrfToken(): string {
 }
 
 export const AdminSelectedUserEditPage = ({ user }: Props) => (
-  <div className="row admin-row">
+  <div className='row admin-row'>
     <AdminSideMenu />
-    <div className="col-10 admin-col-9">
-      <h1 className="admin-main-title">ユーザー編集</h1>
+    <div className='col-10 admin-col-9'>
+      <h1 className='admin-main-title'>ユーザー編集</h1>
       <form
         action={`/admin/user/edit/${user.id}`}
-        method="post"
-        encType="multipart/form-data"
+        method='post'
+        encType='multipart/form-data'
         onSubmit={(e) => {
           if (!window.confirm('変更しますか？')) e.preventDefault();
         }}
       >
-        <input type="hidden" name="_method" value="patch" />
-        <input type="hidden" name="authenticity_token" value={getCsrfToken()} />
+        <input type='hidden' name='_method' value='patch' />
+        <input type='hidden' name='authenticity_token' value={getCsrfToken()} />
 
         {/* アバター */}
-        <div className="card">
-          <div className="card-body">
+        <div className='card'>
+          <div className='card-body'>
             <img
               src={user.avatarPath}
-              className="img-circle"
-              alt="アバター画像"
+              className='img-circle'
+              alt='アバター画像'
               style={{ width: '100%', height: '100%' }}
             />
             <input
-              type="file"
-              name="user[avatar_path]"
-              className="form-control-file"
-              id="exampleFormControlFile1"
-              accept="image/jpg,image/jpeg,image/png"
+              type='file'
+              name='user[avatar_path]'
+              className='form-control-file'
+              id='exampleFormControlFile1'
+              accept='image/jpg,image/jpeg,image/png'
             />
           </div>
         </div>
 
         {/* プロフィール */}
-        <div className="card">
-          <div className="card-body">
+        <div className='card'>
+          <div className='card-body'>
             <textarea
-              name="user[profile]"
-              className="form-control"
-              placeholder="紹介文"
+              name='user[profile]'
+              className='form-control'
+              placeholder='紹介文'
               rows={15}
               cols={20}
               defaultValue={user.profile}
@@ -70,9 +70,9 @@ export const AdminSelectedUserEditPage = ({ user }: Props) => (
         </div>
 
         {/* 送信 */}
-        <div className="card text-right">
-          <div className="card-body">
-            <button type="submit" className="btn btn-warning btn-lg">
+        <div className='card text-right'>
+          <div className='card-body'>
+            <button type='submit' className='btn btn-warning btn-lg'>
               変更
             </button>
           </div>
