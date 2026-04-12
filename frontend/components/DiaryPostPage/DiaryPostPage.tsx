da7ae7adbddc4a377378bca88a8c3deb0c062250
Qluqlu3 <plksmcgof@gmail.com>
2026-04-12 16:38:20 +0900

    fix: noArrayIndexKey — DiaryPostPage

--- frontend/components/DiaryPostPage/DiaryPostPage.tsx
+++ frontend/components/DiaryPostPage/DiaryPostPage.tsx
@@ -19,8 +19,8 @@ export const DiaryPostPage = ({ errors, userName, avatarPath }: Props) => {
       {errors.length > 0 && (
         <div id="error_explanation" className="alert alert-danger">
           <ul>
-            {errors.map((msg, i) => (
-              <li key={i}>{msg}</li>
+            {errors.map((msg) => (
+              <li key={msg}>{msg}</li>
             ))}
           </ul>
         </div>
