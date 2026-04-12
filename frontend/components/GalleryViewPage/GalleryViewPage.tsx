975c190392a799af19271458d9a92d01f7700fbd
Qluqlu3 <plksmcgof@gmail.com>
2026-04-12 16:38:46 +0900

    fix: noArrayIndexKey — GalleryViewPage

--- frontend/components/GalleryViewPage/GalleryViewPage.tsx
+++ frontend/components/GalleryViewPage/GalleryViewPage.tsx
@@ -14,8 +14,8 @@ export const GalleryViewPage = ({ errors }: Props) => (
     {errors.length > 0 && (
       <div className="alert alert-danger">
         <ul>
-          {errors.map((msg, i) => (
-            <li key={i}>{msg}</li>
+          {errors.map((msg) => (
+            <li key={msg}>{msg}</li>
           ))}
         </ul>
       </div>
