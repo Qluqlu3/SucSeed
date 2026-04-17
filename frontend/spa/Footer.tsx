183059bfce43243032907f0e51f7a8f9f68b711f
Qluqlu3 <plksmcgof@gmail.com>
2026-04-18 03:09:02 +0900

    style: JSX 属性のクオートスタイルをダブルクオートからシングルクオートに変更

--- frontend/spa/Footer.tsx
+++ frontend/spa/Footer.tsx
@@ -1,10 +1,10 @@
 import type { FC } from 'react';
 
 export const Footer: FC = () => (
-  <footer className="container-fluid original-footer">
-    <a href="/inquiry/input" className="navbar-brand inquiry-link">
+  <footer className='container-fluid original-footer'>
+    <a href='/inquiry/input' className='navbar-brand inquiry-link'>
       お問い合わせ
     </a>
-    <p className="copy-right">&copy;SucSeed</p>
+    <p className='copy-right'>&copy;SucSeed</p>
   </footer>
 );
