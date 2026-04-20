782cdcc7448623b6e0f5da9aa11a0c239b30f33e
Qluqlu3 <plksmcgof@gmail.com>
2026-04-21 00:51:08 +0900

    refactor: navbar 認証アクションを分離

--- frontend/spa/Navbar.tsx
+++ frontend/spa/Navbar.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react';
+import { NavbarAuthAction } from './NavbarAuthAction';
 import { NavbarSearchForm } from './NavbarSearchForm';
 import { getNavbarMenuItems } from './navbarMenu';
-import { PostForm } from './PostForm';
 import type { ArtCategory, Role } from './session';
 
 type NavbarProps = {
@@ -15,41 +15,41 @@ export const Navbar: FC<NavbarProps> = ({ role, artCategories, logoSrc, titleSrc
   const menuItems = getNavbarMenuItems(role);
 
   return (
-    <nav className='navbar navbar-expand-lg my_nav index-nav'>
-      <img src={logoSrc} className='logo' width='50' height='50' alt='ロゴ' />
-      <a href='/index' className='navbar-brand my-brand'>
-        <img src={titleSrc} width='150' alt='SucSeed' />
+    <nav className="navbar navbar-expand-lg my_nav index-nav">
+      <img src={logoSrc} className="logo" width="50" height="50" alt="ロゴ" />
+      <a href="/index" className="navbar-brand my-brand">
+        <img src={titleSrc} width="150" alt="SucSeed" />
       </a>
       <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarSupportedContent'
-        aria-controls='navbarSupportedContent'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
       >
-        <span className='navbar-toggler-icon'>
-          <i className='fas fa-list mini-list' aria-hidden='true' />
+        <span className="navbar-toggler-icon">
+          <i className="fas fa-list mini-list" aria-hidden="true" />
         </span>
       </button>
-      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-        <ul className='navbar-nav mr-auto'>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
           {menuItems && (
-            <li className='nav-item dropdown'>
+            <li className="nav-item dropdown">
               <button
-                type='button'
-                className='nav-link dropdown-toggle'
-                id='navbarDropdown'
-                data-toggle='dropdown'
-                aria-haspopup='true'
-                aria-expanded='false'
+                type="button"
+                className="nav-link dropdown-toggle"
+                id="navbarDropdown"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
               >
                 メニュー
               </button>
-              <div className='dropdown-menu'>
+              <div className="dropdown-menu">
                 {menuItems.map((item) => (
-                  <a key={item.href} href={item.href} className='dropdown-item'>
+                  <a key={item.href} href={item.href} className="dropdown-item">
                     {item.label}
                   </a>
                 ))}
@@ -58,24 +58,9 @@ export const Navbar: FC<NavbarProps> = ({ role, artCategories, logoSrc, titleSrc
           )}
           <NavbarSearchForm artCategories={artCategories} />
         </ul>
-        <ul className='nav navbar-nav navbar-right'>
-          <li className='login-ul'>
-            {role === 'guest' ? (
-              <button
-                type='button'
-                className='btn my-login-btn'
-                data-toggle='modal'
-                data-target='#exampleModalCenter'
-              >
-                ログイン
-              </button>
-            ) : (
-              <PostForm action='/user/logout'>
-                <button type='submit' className='btn my-login-btn'>
-                  ログアウト
-                </button>
-              </PostForm>
-            )}
+        <ul className="nav navbar-nav navbar-right">
+          <li className="login-ul">
+            <NavbarAuthAction role={role} />
           </li>
         </ul>
       </div>
---
+++ frontend/spa/NavbarAuthAction.tsx
@@ -0,0 +1,30 @@
+import type { FC } from 'react';
+import { PostForm } from './PostForm';
+import type { Role } from './session';
+
+type NavbarAuthActionProps = {
+  role: Role;
+};
+
+export const NavbarAuthAction: FC<NavbarAuthActionProps> = ({ role }) => {
+  if (role === 'guest') {
+    return (
+      <button
+        type="button"
+        className="btn my-login-btn"
+        data-toggle="modal"
+        data-target="#exampleModalCenter"
+      >
+        ログイン
+      </button>
+    );
+  }
+
+  return (
+    <PostForm action="/user/logout">
+      <button type="submit" className="btn my-login-btn">
+        ログアウト
+      </button>
+    </PostForm>
+  );
+};
