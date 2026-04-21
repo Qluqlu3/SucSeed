e599d2878a6d119570fae1b1a13a18c75576b586
Qluqlu3 <plksmcgof@gmail.com>
2026-04-22 02:12:06 +0900

    refactor: navbar メニュードロップダウンを分離

--- frontend/spa/Navbar.tsx
+++ frontend/spa/Navbar.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import { NavbarAuthAction } from './NavbarAuthAction';
+import { NavbarMenuDropdown } from './NavbarMenuDropdown';
 import { NavbarSearchForm } from './NavbarSearchForm';
 import { getNavbarMenuItems } from './navbarMenu';
 import type { ArtCategory, Role } from './session';
@@ -15,51 +16,31 @@ export const Navbar: FC<NavbarProps> = ({ role, artCategories, logoSrc, titleSrc
   const menuItems = getNavbarMenuItems(role);
 
   return (
-    <nav className='navbar navbar-expand-lg my_nav index-nav'>
-      <img src={logoSrc} className='logo' width='50' height='50' alt='ロゴ' />
-      <a href='/index' className='navbar-brand my-brand'>
-        <img src={titleSrc} width='150' alt='SucSeed' />
+    <nav className="navbar navbar-expand-lg my_nav index-nav">
+      <img src={logoSrc} className="logo" width="50" height="50" alt="ロゴ" />
+      <a href="/index" className="navbar-brand my-brand">
+        <img src={titleSrc} width="150" alt="SucSeed" />
       </a>
       <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarSupportedContent'
-        aria-controls='navbarSupportedContent'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
       >
-        <span className='navbar-toggler-icon'>
-          <i className='fas fa-list mini-list' aria-hidden='true' />
+        <span className="navbar-toggler-icon">
+          <i className="fas fa-list mini-list" aria-hidden="true" />
         </span>
       </button>
-      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-        <ul className='navbar-nav mr-auto'>
-          {menuItems && (
-            <li className='nav-item dropdown'>
-              <button
-                type='button'
-                className='nav-link dropdown-toggle'
-                id='navbarDropdown'
-                data-toggle='dropdown'
-                aria-haspopup='true'
-                aria-expanded='false'
-              >
-                メニュー
-              </button>
-              <div className='dropdown-menu'>
-                {menuItems.map((item) => (
-                  <a key={item.href} href={item.href} className='dropdown-item'>
-                    {item.label}
-                  </a>
-                ))}
-              </div>
-            </li>
-          )}
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          {menuItems && <NavbarMenuDropdown menuItems={menuItems} />}
           <NavbarSearchForm artCategories={artCategories} />
         </ul>
-        <ul className='nav navbar-nav navbar-right'>
-          <li className='login-ul'>
+        <ul className="nav navbar-nav navbar-right">
+          <li className="login-ul">
             <NavbarAuthAction role={role} />
           </li>
         </ul>
---
+++ frontend/spa/NavbarMenuDropdown.tsx
@@ -0,0 +1,28 @@
+import type { FC } from 'react';
+import type { NavbarMenuItem } from './navbarMenu';
+
+type NavbarMenuDropdownProps = {
+  menuItems: NavbarMenuItem[];
+};
+
+export const NavbarMenuDropdown: FC<NavbarMenuDropdownProps> = ({ menuItems }) => (
+  <li className="nav-item dropdown">
+    <button
+      type="button"
+      className="nav-link dropdown-toggle"
+      id="navbarDropdown"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      メニュー
+    </button>
+    <div className="dropdown-menu">
+      {menuItems.map((item) => (
+        <a key={item.href} href={item.href} className="dropdown-item">
+          {item.label}
+        </a>
+      ))}
+    </div>
+  </li>
+);
