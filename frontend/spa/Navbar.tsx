9397bc2767430b90b07089b69fb297375a593a26
Qluqlu3 <plksmcgof@gmail.com>
2026-04-22 18:48:17 +0900

    refactor: navbar ブランド表示を分離

--- frontend/spa/Navbar.tsx
+++ frontend/spa/Navbar.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import { NavbarAuthAction } from './NavbarAuthAction';
+import { NavbarBrand } from './NavbarBrand';
 import { NavbarMenuDropdown } from './NavbarMenuDropdown';
 import { NavbarSearchForm } from './NavbarSearchForm';
 import { getNavbarMenuItems } from './navbarMenu';
@@ -16,31 +17,28 @@ export const Navbar: FC<NavbarProps> = ({ role, artCategories, logoSrc, titleSrc
   const menuItems = getNavbarMenuItems(role);
 
   return (
-    <nav className='navbar navbar-expand-lg my_nav index-nav'>
-      <img src={logoSrc} className='logo' width='50' height='50' alt='ロゴ' />
-      <a href='/index' className='navbar-brand my-brand'>
-        <img src={titleSrc} width='150' alt='SucSeed' />
-      </a>
+    <nav className="navbar navbar-expand-lg my_nav index-nav">
+      <NavbarBrand logoSrc={logoSrc} titleSrc={titleSrc} />
       <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarSupportedContent'
-        aria-controls='navbarSupportedContent'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
       >
-        <span className='navbar-toggler-icon'>
-          <i className='fas fa-list mini-list' aria-hidden='true' />
+        <span className="navbar-toggler-icon">
+          <i className="fas fa-list mini-list" aria-hidden="true" />
         </span>
       </button>
-      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-        <ul className='navbar-nav mr-auto'>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
           {menuItems && <NavbarMenuDropdown menuItems={menuItems} />}
           <NavbarSearchForm artCategories={artCategories} />
         </ul>
-        <ul className='nav navbar-nav navbar-right'>
-          <li className='login-ul'>
+        <ul className="nav navbar-nav navbar-right">
+          <li className="login-ul">
             <NavbarAuthAction role={role} />
           </li>
         </ul>
