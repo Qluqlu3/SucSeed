7c096fd4d316aad575eb48485ba51fe21b68085a
Qluqlu3 <plksmcgof@gmail.com>
2026-04-23 00:12:33 +0900

    refactor: LoginModal headerとfooterを分離

---
+++ frontend/spa/LoginModalFooter.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+export const LoginModalFooter: FC = () => (
+  <div className="modal-footer">
+    <div>
+      <a href="/user/create" className="btn create-user-btn">
+        ユーザ登録はこちらから
+      </a>
+    </div>
+  </div>
+);
---
+++ frontend/spa/LoginModalHeader.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react';
+
+export const LoginModalHeader: FC = () => (
+  <div className="modal-header">
+    <h5 className="modal-title" id="exampleModalLongTitle">
+      ログインフォーム
+    </h5>
+    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+);
