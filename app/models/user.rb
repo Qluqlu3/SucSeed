9cc240699cf4c163bd4898863393cc65a1b4a499
Qluqlu3 <plksmcgof@gmail.com>
2026-05-10 22:02:44 +0900

    refactor: user.rb の has_many 重複定義を別名に分割

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,22 +8,21 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, on: :create
   validates :password, allow_nil: true, presence: true, confirmation: true, length: { in: 8..16 }, format: {with: /[a-zA-Z0-9]/}
   has_one :creator
+  has_one :heir
   has_many :diaries
   has_many :diary_comments
   has_many :diary_goods
-  has_many :inquiries
-  has_many :favorites
-  has_many :messages, class_name: 'Message', :foreign_key => 'send_user_id'
-  has_many :messages, class_name: 'Message', :foreign_key => 'receive_user_id'
-  has_many :favorites, class_name: 'Favorite', :foreign_key => 'user_id'
-  has_many :favorites, class_name: 'Favorite', :foreign_key => 'favorite_user_id'
   has_many :galleries
-  has_many :matches, class_name: 'Match', :foreign_key => 'target_user_id'
-  has_many :matches, class_name: 'Match', :foreign_key => 'user_id'
-  has_one :heir
-  has_many :message_lists, class_name: 'MessageList', :foreign_key => 'creator_user_id'
-  has_many :message_lists, class_name: 'MessageList', :foreign_key => 'heir_user_id'
   has_many :gallery_goods
   has_many :gallery_comments
+  has_many :inquiries
+  has_many :favorites, class_name: 'Favorite', foreign_key: :user_id
+  has_many :favorited_by, class_name: 'Favorite', foreign_key: :favorite_user_id
+  has_many :sent_messages, class_name: 'Message', foreign_key: :send_user_id
+  has_many :received_messages, class_name: 'Message', foreign_key: :receive_user_id
+  has_many :sent_matches, class_name: 'Match', foreign_key: :user_id
+  has_many :target_matches, class_name: 'Match', foreign_key: :target_user_id
+  has_many :creator_message_lists, class_name: 'MessageList', foreign_key: :creator_user_id
+  has_many :heir_message_lists, class_name: 'MessageList', foreign_key: :heir_user_id
   acts_as_tagger
 end
