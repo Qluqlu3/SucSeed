137e4d0a885d88921646afdbb8ead2b67188429e
Qluqlu3 <plksmcgof@gmail.com>
2026-05-31 15:43:33 +0900

    refactor: Error404Page と Error500Page の JSX インデントを統一

--- frontend/components/Error500Page/Error500Page.tsx
+++ frontend/components/Error500Page/Error500Page.tsx
@@ -4,10 +4,13 @@
 
 export const Error500Page = () => (
   <div>
-    <h1 className="error-title error-page-title">エラーが発生しました</h1>
-    <div className="error-page-box">
-      <div className="error-btn-box">
-        <a href="/index" className="inline-block rounded bg-gray-200 px-5 py-2 text-lg hover:opacity-80">
+    <h1 className='error-title error-page-title'>エラーが発生しました</h1>
+    <div className='error-page-box'>
+      <div className='error-btn-box'>
+        <a
+          href='/index'
+          className='inline-block rounded bg-gray-200 px-5 py-2 text-lg hover:opacity-80'
+        >
           トップページ
         </a>
       </div>
