885e9a80c13c42c443239c055d542b6e709cfc25
Qluqlu3 <plksmcgof@gmail.com>
2026-08-13 00:54:42 +0900

    fix: 死んでいるCSP/ActiveStorage設定を整理
    content_security_policy.rbが全行コメントアウトのままcsp_meta_tagだけ
    呼ばれており、CSPヘッダーが実質無効化されていた。外部CDN・インライン
    script/styleを使わない構成であることを確認した上で、Sentryへの通信のみ
    connect-srcで許可する実ポリシーを有効化。
    
    また、has_one_attached等のActiveStorage利用が皆無(アップロードは全面
    CarrierWave)なのに各環境でconfig.active_storage.serviceだけが
    残っていたため削除(参照が無くなったconfig/storage.ymlは前コミットで削除済み)。

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,20 +4,20 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
-
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
-
-# If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+# アプリはesbuildが出力する同一オリジンのバンドルのみを読み込み、外部CDNやインラインscript/styleは使わない
+# （フロントはReactのCSSOM経由のstyleなのでstyle-src :selfで問題ない）。
+# 例外はSentry(frontend/spa/sentry.ts)のエラーレポート送信先のみ。
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self
+  policy.font_src    :self
+  policy.img_src     :self, :data
+  policy.object_src  :none
+  policy.script_src  :self
+  policy.style_src   :self
+  policy.connect_src :self, 'https://*.ingest.sentry.io', 'https://*.ingest.us.sentry.io', 'https://*.ingest.de.sentry.io'
+  policy.base_uri    :self
+  policy.frame_ancestors :none
+end
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
